3dff51b0abc7575f69c1fb6f282449093cce2da1
anchuri-sindhuja <112188850+anchuri-sindhuja@users.noreply.github.com>
2022-11-11 12:32:02 -0600

    Create resource.js

---
+++ routes/resource.js
@@ -0,0 +1,31 @@
+var express = require('express'); 
+var router = express.Router(); 
+
+// Require controller modules. 
+var api_controller = require('../controllers/api'); 
+var hat_controller = require('../controllers/hat'); 
+
+/// API ROUTE /// 
+
+// GET resources base. 
+router.get('/', api_controller.api); 
+
+/// hat ROUTES /// 
+
+// POST request for creating a hat.  
+router.post('/hat', hat_controller.hat_create_post); 
+
+// DELETE request to delete hat. 
+router.delete('/hat/:id', hat_controller.hat_delete); 
+
+// PUT request to update hat. 
+router.put('/hat/:id', hat_controller.hat_update_put); 
+
+// GET request for one hat. 
+router.get('/hat/:id', hat_controller.hat_detail); 
+
+// GET request for list of all hat items. 
+router.get('/hat', hat_controller.hat_list); 
+
+module.exports = router; 
+
