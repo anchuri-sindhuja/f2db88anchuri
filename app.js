42c08df52be5e87118ff471839b8039aaed349f8
anchuri-sindhuja <112188850+anchuri-sindhuja@users.noreply.github.com>
2022-11-28 13:20:27 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
 
-
-require('dotenv').config(); 
+passport.use(new LocalStrategy)( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } 
+  );
+require('dotenv').config();
 const connectionString =  process.env.MONGO_CON 
 mongoose = require('mongoose'); 
 mongoose.connect(connectionString,  
@@ -28,9 +43,6 @@ var gridbuildRouter = require('./routes/gridbuild');
 var selectorRouter = require('./routes/selector');
 const hat = require('./models/hat');
 var resourceRouter = require('./routes/resource');
-
-
-
 var app = express();
 
 // view engine setup
@@ -41,6 +53,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -105,6 +124,17 @@ let reseed = true;
 if (reseed) {
    recreateDB();
 } 
+
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+
+
  
 
 module.exports = app;
